85b039fe454a10f74643d309f5127170b1b8be4e
cnwneal <cneal2@luc.edu>
2016-11-23 21:06:28 -0600

    freestanding C

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 nasm -f elf32 kernel.asm -o kasm.o
-gcc -m32 -c kernel.c -o kc.o -ffreestanding
+gcc -m32 -c kernel.c -o kc.o -ffreestanding                        #using the free standing version of C which means there is no standard library on the code I give it.
 ld -m elf_i386 -T link.ld -o comp410OS/boot/kernel.bin kasm.o kc.o 
 qemu-system-i386 -kernel comp410OS/boot/kernel.bin
 #grub-mkrescue -o comp410OS.iso include/
